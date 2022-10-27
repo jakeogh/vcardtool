f9d98c1baeaca06b9e13d8d94daf5e5f42a8840d
jakeogh <github.com@v6y.net>
2022-10-26 18:10:12 -0700

    auto-commit

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import sys
 
 from setuptools import find_packages
 from setuptools import setup
 
 import fastentrypoints
-
-if not sys.version_info[0] == 3:
-    sys.exit("Python 3 is required. Use: \'python3 setup.py install\'")
 
 dependencies = ["click"]
 
@@ -21,8 +17,8 @@ config = {
     "author_email": "github.com@v6y.net",
     "description": "Split a multiple record vcard .vcf file into individual .vcf files",
     "long_description": __doc__,
-    "packages": find_packages(exclude=['tests']),
-    "package_data": {"vcardtool": ['py.typed']},
+    "packages": find_packages(exclude=["tests"]),
+    "package_data": {"vcardtool": ["py.typed"]},
     "include_package_data": True,
     "zip_safe": False,
     "platforms": "any",
